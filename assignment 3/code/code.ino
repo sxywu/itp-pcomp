efdb55fb29f4b981efffb2a82c37a505ca21e607
Shirley Wu <shirleyxueyangwu@gmail.com>
2021-11-28 22:33:53 -0500

    project 3: start on brightness for animation

--- assignment 3/code/code.ino
+++ assignment 3/code/code.ino
@@ -4,11 +4,15 @@
 // 5 photoresistors, 1 button for resetting
 #define numSensors 2
 #define buttonPin 2
+// how much to increase/decrease by
+#define margin 2
 
 // array of sensor pins
 const int sensorPins[numSensors] = {17, 21};
 // array of default/normalized sensor readings
 int sensorLows[numSensors];
+// array of flower animation percentages
+int animationPercents[numSensors] = {0, 0};
 // remember reset button state (HIGH bc pullup resistor)
 int prevButtonState = HIGH;
 
@@ -29,7 +33,43 @@ void loop() {
   }
   prevButtonState = buttonState;
 
-  // then read sensors
+  calcAnimationPercents();
+
+  // send animation values
+  for (int sensor = 0; sensor < numSensors; sensor += 1) {
+    Serial.print(animationPercents[sensor]);
+    if (sensor < numSensors - 1) {
+      Serial.print(",");
+    } else {
+      Serial.println();
+    }
+  }
+
+  delay(50);
+}
+
+// read each sensor and calculate what % of animation
+// the corresponding flower should be at
+void calcAnimationPercents() {
+  for (int sensor = 0; sensor < numSensors; sensor += 1) {
+    // 1. read sensor
+    int sensorPin = sensorPins[sensor];
+    int brightness = analogRead(sensorPin);
+    
+    // 2. map brightness to number between 0 and 100
+    brightness = map(brightness, sensorLows[sensor] + 50, 1000, 0, 100);
+    brightness = constrain(brightness, 0, 100);
+    
+    // 3. compare brightness to current animation percent
+    if (animationPercents[sensor] < brightness) {
+      // current reading is bigger so add 5% and constrain to 100%
+      animationPercents[sensor] = min(animationPercents[sensor] + margin, 100);
+    } else if (animationPercents[sensor] > brightness) {
+      animationPercents[sensor] = max(0, animationPercents[sensor] - margin);
+    }
+
+    delay(10);
+  }
 }
 
 // for each sensor, read 20 values and remember average
